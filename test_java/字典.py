70ef0b0d2b2b92ef5ec4226e6aeb99d8ae2c19ea
algae <xzyim@outlook.com>
2020-04-20 18:13:31 +0800

    修改了一次

--- test_java/字典.py
+++ test_java/字典.py
@@ -1,6 +1,6 @@
 person = {'name': '欧阳娜娜', 'sex': '女', 'age': 20}
 for p in person:
-    print(person.get(p,'这是默认值'))
+    print(person.get(p,'这是默认值啊'))
 
 person_keys = person.keys()
 person_value = person.values()
